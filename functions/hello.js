8d91749d77cc4ee8565e31fc147f15bf6a906a2f
bhavinsen <senbhavin@gmail.com>
2023-01-30 16:04:09 +0530

    initial commit

---
+++ functions/hello.js
@@ -0,0 +1,9 @@
+
+
+//.netlify/functions/hello.js
+exports.handler = async function () {
+    return {
+      statusCode: 200,
+      body: 'Hello world!',
+    }
+  }
---
+++ src/pages/AboutPage.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import styled from 'styled-components'
+import { PageHero } from '../components'
+import aboutImg from '../assets/hero-bcg.jpeg'
+
+const AboutPage = () => {
+  return <main>
+    <PageHero title='about'/>
+    <Wrapper className='page section section-center'>
+      <img src={aboutImg} alt='nice desk'/>
+      <article>
+        <div className="title">
+          <h2>our story</h2>
+          <div className="underline"></div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus suscipit earum asperiores, corporis doloremque, voluptates ab officiis totam dolores delectus ea mollitia tenetur vitae aspernatur necessitatibus et odit illum quibusdam officia accusamus sapiente eligendi? Corporis a laborum ex, aut beatae repellat et tenetur? Accusamus placeat quia corrupti quasi ab officia!
+          </p>
+        </div>
+      </article>
+    </Wrapper>
+  </main>
+}
+
+const Wrapper = styled.section`
+  display: grid;
+  gap: 4rem;
+  img {
+    width: 100%;
+    display: block;
+    border-radius: var(--radius);
+    height: 500px;
+    object-fit: cover;
+  }
+  p {
+    line-height: 2;
+    max-width: 45em;
+    margin: 0 auto;
+    margin-top: 2rem;
+    color: var(--clr-grey-5);
+  }
+  .title {
+    text-align: left;
+  }
+  .underline {
+    margin-left: 0;
+  }
+  @media (min-width: 992px) {
+    grid-template-columns: 1fr 1fr;
+  }
+`
+export default AboutPage
